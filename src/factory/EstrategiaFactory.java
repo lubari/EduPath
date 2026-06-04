86f8c3612bbf085bfb05f0b599c0c99d256ea578
Ana <analubari@gmail.com>
2026-06-04 19:24:29 -0300

    commit

---
+++ src/factory/EstrategiaFactory.java
@@ -0,0 +1,8 @@
+package factory;
+
+public class EstrategiaFactory {
+
+    public void crear(String tipo){
+        return;
+    }
+}
--- src/service/RecomendacionService.java
+++ src/service/RecomendacionService.java
@@ -1,5 +1,6 @@
 package service;
 
+import interfaces.IAlumnoRepository;
 import model.Alumno;
 import model.Estadisticas;
 import model.Materia;
@@ -9,6 +10,7 @@ import java.util.List;
 
 public class RecomendacionService {
     private EstrategiaFactory estrategiaFactory;
+    private IAlumnoRepository alumnoRepository;
 
     public Recomendacion generarPlan(Alumno alumno){
         // Implementar lógica para generar un plan de estudio basado en el alumno
